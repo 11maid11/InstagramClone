c0de6bc825eae57d5ccb4a71432e42749f1bedf4
Maid <maid_rondic@live.com>
2018-10-27 22:36:25 +0100

    fancy toast

--- app/src/main/java/com/maid/instagramclone/SignUp.java
+++ app/src/main/java/com/maid/instagramclone/SignUp.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,18 +24,59 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     private EditText edtName;
     private EditText edtKickSpd;
     private EditText edtKickPwr;
-
+    private String data;
+    private TextView txtGetData;
+    private Button getAllData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getAllData = findViewById(R.id.btnGetAll);
         btnSave = findViewById(R.id.btnSubmit);
         edtName = findViewById(R.id.edtName);
         edtKickPwr = findViewById(R.id.edtKickPwr);
         edtKickSpd = findViewById(R.id.edtkickSpd);
+        txtGetData = findViewById(R.id.txtGetData);
         btnSave.setOnClickListener(this);
+        getAllData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                data = "";
+                final ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("KickBoxer");
+                queryAll.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if(e ==null ){
+                            if(objects.size()>0){
+                                FancyToast.makeText(SignUp.this,"Success",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,false).show();
+                                for (int i =0;i<objects.size();i++){
+                                    data+= objects.get(i).get("name") + ";" + objects.get(i).get("punch_power");
+                                }
+                            }
+                        }else{
+                            FancyToast.makeText(SignUp.this,"Error occured",FancyToast.LENGTH_SHORT,FancyToast.ERROR,false).show();
+                        }
+                        txtGetData.setText(data);
+                    }
+                });
+            }
+        });
+        txtGetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("KickBoxer");
+                parseQuery.getInBackground("dnVzzlN4Ay", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if(object != null && e == null){
+                            txtGetData.setText(object.get("name") + " " +object.get("punch_power"));
+                        }
+                    }
+                });
 
+            }
+            });
     }
 
     @Override
